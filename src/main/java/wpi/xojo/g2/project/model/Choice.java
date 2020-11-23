3d5ecbb00df64f33ac02509ec643d5ef077ee19a
SeanJan0 <60043935+SeanJan0@users.noreply.github.com>
2020-11-24 02:35:35 +0800

    Update Choice.java

--- src/main/java/wpi/xojo/g2/project/model/Choice.java
+++ src/main/java/wpi/xojo/g2/project/model/Choice.java
@@ -1,5 +1,33 @@
 package wpi.xojo.g2.project.model;
 
-public class Choice {
+import java.util.Date;
 
+public class Choice {
+	public final String choiceID;
+	public final String name;
+	public final String desc;
+	public final Date date;
+	
+	public Choice(String ID, String name, String desc, Date date) {
+		this.choiceID = ID;
+		this.name = name;
+		this.desc = desc;
+		this.date = date;
+	}
+	
+	public boolean equals (Object o) {
+		if (o == null) { return false; }
+		
+		if (o instanceof Choice) {
+			Choice other = (Choice) o;
+			return name.equals(other.choiceID);
+		}
+		
+		return false;
+	}
+	
+	public String toString() {
+		return "Choice ID: " + choiceID;
+	}
+	
 }
