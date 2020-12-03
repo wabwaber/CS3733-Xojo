5064799c44b33f8cf81d8da6eb2a76a954e7eb96
Ted Clifford <efclifford@wpi.edu>
2020-12-03 11:30:46 -0500

    Implemented vote casting

--- html/js/api.js
+++ html/js/api.js
@@ -6,3 +6,5 @@ var login_url = base_url + "https://okxc9yp9fb.execute-api.us-east-2.amazonaws.c
 var list_alt_url = base_url + "https://y77z5fzyb1.execute-api.us-east-2.amazonaws.com/default/GetAlternatives";
 var get_choice_url = base_url + "https://mpic0e5zcl.execute-api.us-east-2.amazonaws.com/default/GetChoice";
 var set_vote_url = base_url + "https://08zq4xrt56.execute-api.us-east-2.amazonaws.com/default/SetVotes";
+var change_vote_url = base_url + "https://o3h8rvze7i.execute-api.us-east-2.amazonaws.com/default/ChangeVote";
+
