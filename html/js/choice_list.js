3a74a057f120f1ac2b5236abaa5bdb24efe5bab1
gabe camacho <68162863+gabestuf@users.noreply.github.com>
2020-12-03 22:20:48 -0500

    Update choice_list.js

--- html/js/choice_list.js
+++ html/js/choice_list.js
@@ -14,23 +14,39 @@ class ChoiceList extends React.Component {
     console.log("JS:" + js);
 
     var xhr = new XMLHttpRequest();
-    // xhr.open("GET", list_alt_url, true);
-    // xhr.send(js);
 
-    // xhr.onloadend = function () {
-    //     console.log(xhr);
-    //     if (xhr.readyState == XMLHttpRequest.DONE) {
-    //         if (xhr.status == 200) {
-    //             console.log("XHR:" + xhr.responseText);
-    //             // Do something with xhr.responseText
-    //         } else {
-    //             console.log("actual:" + xhr.responseText)
-    //             var js = JSON.parse(xhr.responseText);
-    //             var err = js["response"];
-    //             alert (err);
-    //         }
-    //     }
-    // };
+    xhr.open("GET", get_all_choices_url, true);
+    xhr.send(js);
+
+    xhr.onloadend = function () {
+        console.log(xhr);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            if (xhr.status == 200) {
+                console.log("XHR:" + xhr.responseText);
+
+                var js = JSON.parse(xhr.responseText);
+            console.log(js["list"]);
+
+            var i = 0;
+		for (const choice of js["list"]) {
+                console.log("Val of i: " + i);
+                console.log(choice["choiceID"]);
+                this.state.alternatives.push(React.createElement(Choice, 
+                    {id: choice["choiceID"], 
+                    description: choice["description"],
+                    creation_date: choice["creationDate"]
+                }))
+                i++;
+            }
+
+            } else {
+                console.log("actual:" + xhr.responseText)
+                var js = JSON.parse(xhr.responseText);
+                var err = js["response"];
+                alert (err);
+            }
+        }
+     };
 
     console.log("Sending request");
     return React.createElement(
