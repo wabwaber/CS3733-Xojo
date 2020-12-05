6e647e82919e5b895c8519e515327c9fc86fa281
Ted Clifford <efclifford@wpi.edu>
2020-12-05 16:49:06 -0500

    Fixed indentation

--- html/js/choice_list.js
+++ html/js/choice_list.js
@@ -1,64 +1,60 @@
 'use strict';
 
 class ChoiceList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { choices: [] };
+    }
 
-  render() {
-    var data = {}
-    data["choiceID"] = window.location.pathname;
+    render() {
+        var data = {}
+        data["choiceID"] = window.location.pathname;
 
-    var js = JSON.stringify(data);
-    console.log("JS:" + js);
+        var js = JSON.stringify(data);
+        console.log("JS:" + js);
 
-    var xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", get_all_choices_url, true);
-    xhr.send(js);
+        xhr.open("GET", get_all_choices_url, true);
+        xhr.send(js);
 
-    xhr.onloadend = function () {
-        console.log(xhr);
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status == 200) {
-                console.log("XHR:" + xhr.responseText);
+        xhr.onloadend = function () {
+            console.log(xhr);
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                if (xhr.status == 200) {
+                    console.log("XHR:" + xhr.responseText);
 
-                var js = JSON.parse(xhr.responseText);
-            console.log(js["list"]);
+                    var js = JSON.parse(xhr.responseText);
 
-            var i = 0;
-		for (const choice of js["list"]) {
-                console.log("Val of i: " + i);
-                console.log(choice["choiceID"]);
-                this.state.alternatives.push(React.createElement(Choice, 
-                    {id: choice["choiceID"], 
-                    description: choice["description"],
-                    creation_date: choice["dateCreated"]
-                }))
-                i++;
+                    for (const choice of js["list"]) {
+                        console.log("Val of i: " + i);
+                        console.log(choice["choiceID"]);
+                        this.state.choices.push(React.createElement(Choice, 
+                            { id: choice["choiceID"], 
+                            description: choice["description"],
+                            creation_date: choice["dateCreated"] }))
+                    }
+
+                } else {
+                    console.log("actual:" + xhr.responseText)
+                    var js = JSON.parse(xhr.responseText);
+                    var err = js["response"];
+                    alert (err);
+                }
             }
+        };
 
-            } else {
-                console.log("actual:" + xhr.responseText)
-                var js = JSON.parse(xhr.responseText);
-                var err = js["response"];
-                alert (err);
-            }
-        }
-     };
-
-    console.log("Sending request");
-    return React.createElement(
-      'div',
-      { },
-      React.createElement(
+        console.log("Sending request");
+        return React.createElement(
         'div',
         { },
-        'Choices will appear here'
-      )
-    );
-  }
+        React.createElement(
+            'div',
+            { },
+            'Choices will appear here'
+        )
+        );
+    }
 }
 
 document.querySelectorAll('.choice_list')
