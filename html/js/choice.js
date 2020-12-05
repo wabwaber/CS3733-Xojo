d045f9e1f512a963f4f64c189e6d42a74587a4aa
Ted Clifford <efclifford@wpi.edu>
2020-12-05 17:40:20 -0500

    Added borders

--- html/js/choice.js
+++ html/js/choice.js
@@ -6,36 +6,24 @@ class Choice extends React.Component {
         this.state = { id: props.id, description: props.description, creation_date: props.creation_date, choice_name: props.choice_name, choice_desc: props.choice_desc};
     }
 
-
-  render() {
+    render() {
         var date = new Date(this.state.creation_date); 
 
         return (
-            <div className="choice_box">
-                <div className="choice_name">
-                    <h2>{this.state.choice_name}</h2>
+            <div className="choice_box" style={{margin: "5px", border: "solid", position: "relative", display: "block", overflow: "auto", overflowX: "hidden"}}>
+                <div className="choice_name" style={{margin: "5px"}}>
+                    <h2>{"Name: " + this.state.choice_name}</h2>
                 </div>
-                <div className="choice_id">
-                    <p>{this.state.id}</p>
+                <div className="choice_id" style={{margin: "5px"}}>
+                    <p>{"ID: " + this.state.id}</p>
                 </div>
-                <div className="choice_date">
-                    <p>{date.toString()}</p>
+                <div className="choice_date" style={{margin: "5px"}}>
+                    <p>{"Created: " + date.toString()}</p>
                 </div>
-                <div className="choice_is_completed">
-                    <p>{"not completed"}</p>
+                <div className="choice_is_completed" style={{margin: "5px"}}>
+                    <p>{"Completed? " + "false"}</p>
                 </div>
             </div>
         );
     }
 }
-
-// Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.choice')
-  .forEach(domContainer => {
-    // Read the comment ID from a data-* attribute.
-    const commentID = domContainer.dataset.commentid;
-    ReactDOM.render(
-      React.createElement(Choice, { commentID: commentID }),
-      domContainer
-    );
-  });
